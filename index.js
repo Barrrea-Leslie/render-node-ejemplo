08cef106033176fb5a1a6f003f796f9451553f64
Barrera-Leslie <barreraleslie180294@gmail.com>
2025-11-15 18:55:51 -0700

    cambio

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import express from 'express';
 
-const app = express()
+const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Offroad Kantapon')
-})
+    res.send('Offroad Kantapon');
+});
 
 const PORT = process.env.PORT || 3000;
 
